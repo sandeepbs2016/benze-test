cc4af8b7970c591105f2718165d13900426b47ca
sandeepbs2016 <bs.sandeep2016@gmail.com>
2021-10-11 21:04:12 +0530

    Modified to handle errors

--- src/main/java/com/benze/assignment/entity/Response.java
+++ src/main/java/com/benze/assignment/entity/Response.java
@@ -5,88 +5,22 @@ import java.util.List;
 
 public class Response {
 
-	private String transactionId;
-	private String vin;
-	private String source;
-	private String destination;
-	
-	private int distance;
-	private int currentChargeLevel;
-	
-	private boolean isChargingRequired;
-	
-	private List<String> chargingStations = new ArrayList<>();
-	
+	private long transactionId;
+
 	private List<Errors> errors = new ArrayList<>();
 
-	public String getTransactionId() {
+	/**
+	 * @return the transactionId
+	 */
+	public long getTransactionId() {
 		return transactionId;
 	}
 
-	public void setTransactionId(String transactionId) {
+	/**
+	 * @param transactionId the transactionId to set
+	 */
+	public void setTransactionId(long transactionId) {
 		this.transactionId = transactionId;
-	}
-
-	public String getVin() {
-		return vin;
-	}
-
-	public void setVin(String vin) {
-		this.vin = vin;
-	}
-
-	public String getSource() {
-		return source;
-	}
-
-	public void setSource(String source) {
-		this.source = source;
-	}
-
-	public String getDestination() {
-		return destination;
-	}
-
-	public void setDestination(String destination) {
-		this.destination = destination;
-	}
-
-	public int getDistance() {
-		return distance;
-	}
-
-	public void setDistance(int distance) {
-		this.distance = distance;
-	}
-
-	public int getCurrentChargeLevel() {
-		return currentChargeLevel;
-	}
-
-	public void setCurrentChargeLevel(int currentChargeLevel) {
-		this.currentChargeLevel = currentChargeLevel;
-	}
-
-	public boolean isChargingRequired() {
-		return isChargingRequired;
-	}
-
-	public void setChargingRequired(boolean isChargingRequired) {
-		this.isChargingRequired = isChargingRequired;
-	}
-
-	/**
-	 * @return the chargingStations
-	 */
-	public List<String> getChargingStations() {
-		return chargingStations;
-	}
-
-	/**
-	 * @param chargingStations the chargingStations to set
-	 */
-	public void setChargingStations(List<String> chargingStations) {
-		this.chargingStations = chargingStations;
 	}
 
 	/**
@@ -102,6 +36,5 @@ public class Response {
 	public void setErrors(List<Errors> errors) {
 		this.errors = errors;
 	}
-
 	
 }
